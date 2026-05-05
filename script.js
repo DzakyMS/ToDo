20f055787067313b9c3bc480eb1824dd4fa7a97b
dzaky <email>
2026-05-06 02:00:12 +0700

    commit pertama

---
+++ script.js
@@ -0,0 +1,73 @@
+const tombolSimpan = document.querySelector("#tombol-simpan")
+const inputTanggal = document.querySelector("#input-tanggal")
+const inputAktivitas = document.querySelector("#input-aktivitas")
+const tabelTodo = document.querySelector("#tabel-todo")
+
+
+const style = "col-1 bg-info-subtle border-dark border"
+let modeEdit = {
+    aktif: false,
+    row: null
+}
+
+function urutkan() {
+    let angka = 1;
+    for(row of tabelTodo.children) {
+        if(row.id !== 'head') {
+            row.children[0].innerHTML = angka;
+            angka += 1;
+        }
+    }
+}
+
+function simpanData() {
+    const data = {
+        aktivitas: inputAktivitas.value,
+        tanggal: inputTanggal.value
+    };
+    const row = document.createElement('div');
+
+
+    row.className = "row";
+    for(let i = 1; i <= 5; ++i) {
+        let div = document.createElement('div')
+        div.className = style
+        row.append(div)
+    }
+        
+    row.children[1].innerHTML = data.aktivitas;
+    row.children[2].innerHTML = data.tanggal;
+
+    const checkbox = document.createElement('input');
+    checkbox.type = 'checkbox';
+    row.children[3].append(checkbox);
+
+    const tombolHapus = document.createElement('input');
+    tombolHapus.type = 'button'; tombolHapus.value = 'hapus';
+    tombolHapus.addEventListener('click', () => { 
+        row.remove();
+        urutkan();
+    });
+
+    const tombolEdit = document.createElement('input');
+    tombolEdit.type = 'button'; tombolEdit.value = 'edit';
+    tombolEdit.addEventListener('click', () => {
+        const dataEdit = {
+            aktivitas: inputAktivitas.value,
+            tanggal: inputTanggal.value
+        };
+        
+        row.children[1].innerHTML = dataEdit.aktivitas;
+        row.children[2].innerHTML = dataEdit.tanggal;
+    });
+
+    row.children[4].append(tombolHapus, tombolEdit);
+
+    tabelTodo.append(row);
+    urutkan();
+    
+}
+
+
+
+tombolSimpan.addEventListener('click', simpanData)
